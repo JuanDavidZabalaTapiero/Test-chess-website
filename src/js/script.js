dcf29c162107704453bdfc3accaa1a55182eb65e
JuanDavidZabalaTapiero <juandavidzabalatapiero@gmail.com>
2024-07-31 17:28:30 -0500

    Al clickear el div con bg verde se cambia el <p> de lugar

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     var id_cuadro_seleccionado = null;
 
-    document.querySelectorAll('.cuadro_blanco').forEach(function (cuadro) {
+    document.querySelectorAll('.cuadro_blanco, .cuadro_negro').forEach(function (cuadro) {
         cuadro.addEventListener('click', function () {
 
             if (cuadro.querySelector('p')) {
@@ -19,6 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
                     div_cuadro_seleccionado.classList.add('cuadro_movimiento_posible');
 
+                    // SI CLICKEO EL DIV CON BG DE COLOR VERDE
                     div_cuadro_seleccionado.addEventListener('click', function () {
 
                         document.getElementById(id_cuadro_seleccionado).classList.remove('cuadro_seleccionado');
@@ -43,7 +44,10 @@ document.addEventListener('DOMContentLoaded', () => {
                     document.getElementById(id_cuadro_seleccionado).classList.toggle('cuadro_seleccionado');
 
                     document.getElementById(parseInt(id_cuadro_seleccionado) + 8).classList.toggle('cuadro_movimiento_posible');
+
+                    id_cuadro_seleccionado = null;
                 } else {
+
                     document.getElementById(id_cuadro_seleccionado).classList.remove('cuadro_seleccionado');
 
                     document.getElementById(parseInt(id_cuadro_seleccionado) + 8).classList.remove('cuadro_movimiento_posible');
@@ -53,7 +57,31 @@ document.addEventListener('DOMContentLoaded', () => {
                     document.getElementById(id_cuadro_seleccionado).classList.add('cuadro_seleccionado');
 
                     // CUADRO POSIBLE
-                    document.getElementById(parseInt(id_cuadro_seleccionado) + 8).classList.add('cuadro_movimiento_posible');
+                    var div_cuadro_seleccionado = document.getElementById(parseInt(id_cuadro_seleccionado) + 8);
+
+                    // CAMBIO EL BG DEL CUADRO POSIBLE 
+                    div_cuadro_seleccionado.classList.add('cuadro_movimiento_posible');
+
+                    // SI CLICKEO EL DIV CON BG DE COLOR VERDE
+                    div_cuadro_seleccionado.addEventListener('click', function () {
+
+                        document.getElementById(id_cuadro_seleccionado).classList.remove('cuadro_seleccionado');
+
+                        // CUADRO POSIBLE
+                        document.getElementById(parseInt(id_cuadro_seleccionado) + 8).classList.remove('cuadro_movimiento_posible');
+
+                        var new_p_div = document.createElement('p');
+
+                        new_p_div.innerText = p_div.innerHTML;
+
+                        p_div.remove();
+
+                        // AÑADO EL P AL DIV SELECCIONADO
+                        div_cuadro_seleccionado.appendChild(new_p_div);
+
+                        id_cuadro_seleccionado = null;
+
+                    });
                 }
             } else {
                 return;
