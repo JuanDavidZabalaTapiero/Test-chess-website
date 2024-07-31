150b01ee3d76a95cbde779951f22f7d744e8967b
JuanDavidZabalaTapiero <juandavidzabalatapiero@gmail.com>
2024-07-31 17:02:44 -0500

    Al clickear un cuadro posible de movimiento se mueve el <p>

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,56 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    var cuadro_seleccionado = null;
+    var id_cuadro_seleccionado = null;
 
     document.querySelectorAll('.cuadro_blanco').forEach(function (cuadro) {
         cuadro.addEventListener('click', function () {
 
-            if (cuadro_seleccionado == null) {
+            if (id_cuadro_seleccionado == null) {
                 cuadro.classList.add('cuadro_seleccionado');
 
-                cuadro_seleccionado = cuadro.id;
+                id_cuadro_seleccionado = cuadro.id;
+
+                // P ADENTRO DEL DIV
+                var p_div = document.getElementById(id_cuadro_seleccionado).querySelector('p');
 
                 // CUADRO POSIBLE
-                document.getElementById(parseInt(cuadro_seleccionado) + 8).classList.add('cuadro_movimiento_posible');
+                var div_cuadro_seleccionado = document.getElementById(parseInt(id_cuadro_seleccionado) + 8);
 
-            } else if (cuadro_seleccionado == cuadro.id) {
-                document.getElementById(cuadro_seleccionado).classList.toggle('cuadro_seleccionado');
+                div_cuadro_seleccionado.classList.add('cuadro_movimiento_posible');
 
-                document.getElementById(parseInt(cuadro_seleccionado) + 8).classList.toggle('cuadro_movimiento_posible');
+                div_cuadro_seleccionado.addEventListener('click', function () {
+
+                    document.getElementById(id_cuadro_seleccionado).classList.remove('cuadro_seleccionado');
+
+                    // CUADRO POSIBLE
+                    document.getElementById(parseInt(id_cuadro_seleccionado) + 8).classList.remove('cuadro_movimiento_posible');
+
+                    var new_p_div = document.createElement('p');
+
+                    new_p_div.innerText = p_div.innerHTML;
+
+                    p_div.remove();
+
+                    // AÑADO EL P AL DIV SELECCIONADO
+                    div_cuadro_seleccionado.appendChild(new_p_div);
+
+                });
+
+            } else if (id_cuadro_seleccionado == cuadro.id) {
+                document.getElementById(id_cuadro_seleccionado).classList.toggle('cuadro_seleccionado');
+
+                document.getElementById(parseInt(id_cuadro_seleccionado) + 8).classList.toggle('cuadro_movimiento_posible');
             } else {
-                document.getElementById(cuadro_seleccionado).classList.remove('cuadro_seleccionado');
+                document.getElementById(id_cuadro_seleccionado).classList.remove('cuadro_seleccionado');
 
-                document.getElementById(parseInt(cuadro_seleccionado) + 8).classList.remove('cuadro_movimiento_posible');
+                document.getElementById(parseInt(id_cuadro_seleccionado) + 8).classList.remove('cuadro_movimiento_posible');
 
-                cuadro_seleccionado = cuadro.id;
+                id_cuadro_seleccionado = cuadro.id;
 
-                document.getElementById(cuadro_seleccionado).classList.add('cuadro_seleccionado');
+                document.getElementById(id_cuadro_seleccionado).classList.add('cuadro_seleccionado');
 
-                document.getElementById(parseInt(cuadro_seleccionado) + 8).classList.add('cuadro_movimiento_posible');
+                // CUADRO POSIBLE
+                document.getElementById(parseInt(id_cuadro_seleccionado) + 8).classList.add('cuadro_movimiento_posible');
             }
         })
     })
